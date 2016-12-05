9540b219c6e63a65a1a1ee1ce481db5f61d4c870
paul <paul604@users.noreply.github.com>
2016-12-05 11:50:08 +0100

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -1,11 +1,12 @@
 # !/bin/bash
 iconv -f ISO-8859-1 -t UTF-8 $1 |
  col -b |
- tr " " "\012" |
- tr '\t' "\012" |
+ tr " |\t" "\012" |
+ #tr '\t' "\012" |
  sed -f commandesSED |
+ fgrep -vf fichiernegatif.txt|
  sort -g |
- uniq -c |
+ uniq -c -i|
  sed -re 's/ /\t/g'|
  sed -re 's/\t\t+//g'|
  sort -nb > out.txt
