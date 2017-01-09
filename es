956277b6e63d6eec615751e64491010d058c3ca2
paul <paul604@users.noreply.github.com>
2017-01-09 11:00:00 +0100

    Add files via upload

---
+++ es
@@ -0,0 +1,11 @@
+El 'éxodo' de Semana Santa'
+
+Alrededor de medio millón de madrileños abandonaron la capital a partir de la tarde de ayer con el fin de aprovechar las jornadas festivas de la Semana Santa.
+
+Aunque la mayor parte de la gente salió por carretera, no se produjeron atascos importantes ni tampoco se registraron accidentes graves.
+
+ Las carreteras más concurridas fueron las de Andalucía y de Valencia.
+
+Los trenes y aviones salieron repletos de viajeros.
+
+Casi todas las plazas estaban reservadas desde hacía varias semanas.
--- reco.sh
+++ reco.sh
@@ -4,17 +4,25 @@ then
   rm rep
 fi
 val=0
+bool=0
 for file in $(ls -1 liste/4000*.txt.tmp); do
 
   val=$(fgrep -cf $1 $file)
   echo $file":"$val >> rep
+  # nbl=$(wc -l $1|sed -re 's/ |[a-z]|[A-Z]|\W//g')
+  # nb=$(($nbl/10))
+  # if (( $val >= $nb )); then
+  if (( $val != 0 )); then
+    bool=1
+  fi
 
 done
-
-sed '1,$s/:/ /g' rep |sort -g -k 2| tail -n 1
-echo "la langue du fichier : "$(sed '1,$s/:/ /g' rep |
+if [ $bool == 1 ]; then
+  sed '1,$s/:/ /g' rep |sort -g -k 2| tail -n 1
+  echo "la langue du fichier : "$(sed '1,$s/:/ /g' rep |
         sort -g -k 2|
         tail -n 1 |
-        cut -c11-|
-        sed -e "s/.txt.tmp//g"|
-        sed -e "s/[0-9]//g")
+        cut -c11-12)
+else
+  echo "la langue du fichier est inconnue"
+fi
