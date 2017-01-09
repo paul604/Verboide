49f6ed8934353702a577612775fcc1f65befa1a5
paul <paul604@users.noreply.github.com>
2017-01-09 11:19:35 +0100

    Add files via upload

--- script2.sh
+++ script2.sh
@@ -4,13 +4,17 @@ iconv -f ISO-8859-1 -t UTF-8 $1 |
  col -b |
  awk '{print tolower($0)}'|
  tr " " "\012" |
+ #remplace " " par saut de ligne
  sed -re 's/\W//g' |
  sort -g |
+ #trie
  uniq -c -i|
  sort -nb |
  sed -re 's/ |[0-9]//g'|
  awk '{print " "($0)" "}' > tmp.tmp
 
+#création de .tmp (ajout de " " av et ap les mot)
 ./liste.sh
 
+#reco de la langue
 ./reco.sh tmp.tmp
