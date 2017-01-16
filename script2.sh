776cc1da5813a9077953e91ba303fde02a1572dd
paul <paul604@users.noreply.github.com>
2017-01-16 08:46:55 +0100

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,20 +1,26 @@
 # !/bin/bash
+if [[ -z "$1" ]];
+then
+  echo "!  \$1=null"
 
-iconv -f ISO-8859-1 -t UTF-8 $1 |
- col -b |
- awk '{print tolower($0)}'|
- tr " " "\012" |
- #remplace " " par saut de ligne
- sed -re 's/\W//g' |
- sort -g |
- #trie
- uniq -c -i|
- sort -nb |
- sed -re 's/ |[0-9]//g'|
- awk '{print " "($0)" "}' > tmp.tmp
+else
 
-#création de .tmp (ajout de " " av et ap les mot)
-./liste.sh
+  iconv -f ISO-8859-1 -t UTF-8 $1 |
+   col -b |
+   awk '{print tolower($0)}'|
+   tr " " "\012" |
+   #remplace " " par saut de ligne
+   sed -re 's/\W//g' |
+   sort -g |
+   #trie
+   uniq -c -i|
+   sort -nb |
+   sed -re 's/ |[0-9]//g'|
+   awk '{print " "($0)" "}' > tmp.tmp
 
-#reco de la langue
-./reco.sh tmp.tmp
+  #création de .tmp (ajout de " " av et ap les mot)
+  ./liste.sh
+
+  #reco de la langue
+  ./reco.sh tmp.tmp
+fi
