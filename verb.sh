c4c1d600240659edf2bd00303b54a025dcc45409
paul <paul604@users.noreply.github.com>
2017-01-22 17:49:15 +0100

    Update verb.sh

--- verb.sh
+++ verb.sh
@@ -4,15 +4,6 @@ if [ ! -e touslesversbeconjugues.txt.tmp ]; then
   sed -r '/^[a-zA-Z]{1,3}$/d' touslesversbeconjugues.txt.tmp1 > touslesversbeconjugues.txt.tmp
   rm touslesversbeconjugues.txt.tmp1
 fi
-#3 cara et + // 20 ver les plus
-# fgrep -xf $1 touslesversbeconjugues.txt.tmp
-# comm -12 $1 touslesversbeconjugues.txt.tmp
 
 fgrep -xf touslesversbeconjugues.txt.tmp $1|
  uniq -c -i
-# sort -nb |
-# sed -re 's/ |[0-9]//g'|
-# awk '{print " "($0)" "}'
-
-
-#fgrep -cvf touslesversbeconjugues.txt.tmp touslesversbeconjugues.txt.tmp1
